caaddf068bd9c20059da403fbee9c3f235256e37
Bhavya Agarwal <bhavyaagarwal@vl965-172-31-142-128.wireless.umass.edu>
2026-03-04 20:21:44 -0500

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     return {
-      base: '/bhavya-agarwal-portfolio/',
+      base: '/Bhavya-agarwal-Portfolio/',
       server: {
         port: 3000,
         host: '0.0.0.0',
